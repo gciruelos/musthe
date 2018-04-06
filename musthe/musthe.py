58d476f592f550ce6273c34c840e6e0d74418a73
Federico Ferri <federico.ferri.it@gmail.com>
2018-04-06 19:41:18 +0200

    move intervals diff to Intervals class

--- musthe/musthe.py
+++ musthe/musthe.py
@@ -127,13 +127,21 @@ class Interval():
 
     For example, 'd8', 'P1', 'A5' are valid intervals. 'P3', '5' are not.
     """
+
+    intervals = {
+                            'P1': 0,            'A1': 1,
+        'd2': 0,  'm2': 1,            'M2': 2,  'A2': 3,
+        'd3': 2,  'm3': 3,            'M3': 4,  'A3': 5,
+        'd4': 4,            'P4': 5,            'A4': 6,
+        'd5': 6,            'P5': 7,            'A5': 8,
+        'd6': 7,  'm6': 8,            'M6': 9,  'A6': 10,
+        'd7': 9,  'm7': 10,           'M7': 11, 'A7': 12,
+        'd8': 11,           'P8': 12,           'A8': 13,
+    }
+
     def __init__(self, interval):
         try:
-            self.semitones = {'P1': 0, 'A1': 1, 'd2': 0, 'm2': 1, 'M2': 2, 'A2': 3,
-                              'd3': 2, 'm3': 3, 'M3': 4, 'A3': 5, 'd4': 4, 'P4': 5,
-                              'A4': 6, 'd5': 6, 'P5': 7, 'A5': 8, 'd6': 7, 'm6': 8,
-                              'M6': 9, 'A6': 10,'d7': 9, 'm7': 10, 'M7': 11, 'A7': 12,
-                              'd8': 11, 'P8': 12}[interval]
+            self.semitones = self.intervals[interval]
         except:
             raise Exception('Could not parse the interval.')
         self.number = int(interval[1])
