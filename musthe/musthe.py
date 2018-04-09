85d8736e4fb8e7d0c6ade4bf0412103ed82d4190
Federico Ferri <federico.ferri.it@gmail.com>
2018-04-09 22:38:20 +0200

    exclude greek modes by default

--- musthe/musthe.py
+++ musthe/musthe.py
@@ -353,23 +353,37 @@ class Scale:
     """
 
     scales = {
-        'major' :           ['P1', 'M2', 'M3', 'P4', 'P5', 'M6', 'M7'],
+        'major':            ['P1', 'M2', 'M3', 'P4', 'P5', 'M6', 'M7'],
         'natural_minor':    ['P1', 'M2', 'm3', 'P4', 'P5', 'm6', 'm7'],
         'harmonic_minor':   ['P1', 'M2', 'm3', 'P4', 'P5', 'm6', 'M7'],
         'melodic_minor':    ['P1', 'M2', 'm3', 'P4', 'P5', 'M6', 'M7'],
+        'major_pentatonic': ['P1', 'M2', 'M3',       'P5', 'M6'],
+        'minor_pentatonic': ['P1',       'm3', 'P4', 'P5',       'm7'],
+        # greek modes:
+        'ionian':           ['P1', 'M2', 'M3', 'P4', 'P5', 'M6', 'M7'],
         'dorian':           ['P1', 'M2', 'm3', 'P4', 'P5', 'M6', 'm7'],
-        'locrian':          ['P1', 'm2', 'm3', 'P4', 'd5', 'm6', 'm7'],
+        'phrygian':         ['P1', 'm2', 'm3', 'P4', 'P5', 'm6', 'm7'],
         'lydian':           ['P1', 'M2', 'M3', 'A4', 'P5', 'M6', 'M7'],
         'mixolydian':       ['P1', 'M2', 'M3', 'P4', 'P5', 'M6', 'm7'],
-        'phrygian':         ['P1', 'm2', 'm3', 'P4', 'P5', 'm6', 'm7'],
-        'major_pentatonic': ['P1', 'M2', 'M3',       'P5', 'M6'],
-        'minor_pentatonic': ['P1',       'm3', 'P4', 'P5',       'm7']
+        'aeolian':          ['P1', 'M2', 'm3', 'P4', 'P5', 'm6', 'm7'],
+        'locrian':          ['P1', 'm2', 'm3', 'P4', 'd5', 'm6', 'm7'],
     }
+    greek_modes = {
+        1: 'ionian',
+        2: 'dorian',
+        3: 'phrygian',
+        4: 'lydian',
+        5: 'mixolydian',
+        6: 'aeolian',
+        7: 'locrian'
+    }
+    greek_modes_set = set(greek_modes.values())
 
     @staticmethod
-    def all():
+    def all(include_greek_modes=False):
         for root in Note.all():
             for name in Scale.scales:
+                if not include_greek_modes and name in Scale.greek_modes_set: continue
                 yield Scale(root, name)
 
     def __init__(self, root, name):
@@ -380,6 +394,7 @@ class Scale:
             raise TypeError('Invalid root note type: {}'.format(type(root)))
         if name not in self.scales:
             raise NameError('No such scale: {}'.format(name))
+
         self.root = root
         self.name = name
         self.intervals = [Interval(i) for i in self.scales[name]]
