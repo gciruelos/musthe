e744b7b2583ad2dce094f1f8ebdf2a2f36a6fe05
Andy Chase <andychase@gmail.com>
2024-03-20 11:21:30 -0400

    Docstring and formatting improvements per #27

--- musthe/musthe.py
+++ musthe/musthe.py
@@ -334,6 +334,10 @@ class Chord:
         'm7b5':   'm7dim5',
     }
     
+    """Maps Musthe chord modifiers to their LilyPond `\chordmode` equivalents. Any items
+    omitted from this list (``min``, ``aug`` and ``dim``, for example) are
+    assumed to use the same value in LilyPond.
+    """
     lilypond_modifiers = {
         'maj': None,
         'min': 'm',
@@ -342,10 +346,6 @@ class Chord:
         'm7dim5': '7.5-',
         'open5':  '1.5.8',        
     }
-    """Maps Musthe chord modifiers to their LilyPond `\chordmode` equivalents. Any items
-    omitted from this list (``min``, ``aug`` and ``dim``, for example) are
-    assumed to use the same value in LilyPond.
-    """
 
     valid_types = list(recipes.keys()) + list(aliases.keys())
 
@@ -395,11 +395,22 @@ class Chord:
             return all(self.notes[i] == other.notes[i]
                        for i in range(len(self.notes)))
     
-    def lilypond_notation(self, duration: Union[str,int] = ""):
+    def lilypond_notation(self, duration: Union[str, int] = "") -> str:
         """Returns the chord as string that can be used in a LilyPond
         ``\chordmode`` block.
         
-        Examples:
+        Parameters
+        ----------
+        duration : Union[str, int]
+            A LilyPond note duration, where `1` is a whole note, `2` is a half note, `4` is a quarter note, et cetera. For a dotted note, include a `.` after the number. (See https://lilypond.org/doc/latest/Documentation/notation/durations.)
+
+        Returns
+        -------
+        str
+            A ``str`` representing the chord in LilyPond chord notation. (See: https://lilypond.org/doc/latest/Documentation/notation/chord-notation)
+
+        Examples
+        --------
             >>> Chord(Note('C')).lilypond_notation()
             'c'
 
